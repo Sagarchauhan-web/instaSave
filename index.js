5490cc0a67e1a023be5418f30369530d4eda2011
sagar chauhan <sagarchawan4646@gmail.com>
2022-03-14 13:23:51 +0530

    update2

--- index.js
+++ index.js
@@ -6,19 +6,23 @@ let insta_video = require('./routes/insta_video');
 let insta_image = require('./routes/insta_image');
 
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({
-    extended: true
-})); // support encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+); // support encoded bodies
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
 });
 
-
 app.get('/', (req, res) => {
-    res.send('Yea, its Running!');
+  res.send('Yea, its Running!');
 });
 
 /**
@@ -31,7 +35,6 @@ app.get('/instagram/video', insta_video);
  */
 app.get('/instagram/image', insta_image);
 
-const port = 8080;
-app.listen(port, ()=>{
-    console.log("Server running at "+port);
+app.listen(process.env.port || 8080, () => {
+  console.log('Server running at ' + port);
 });
